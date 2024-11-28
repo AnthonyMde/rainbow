330c9c84406bcd2fd8463f7e89e85833b921e857
AnthonyMde <anthony.mamode@gmail.com>
2024-11-28 17:15:07 +0100

    Fix android status bar not visible.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
 import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack screenOptions={screenOptions}>
-      <Stack.Screen name="(home)" />
-    </Stack>
+  return (
+    <>
+    {/* TODO: use current theme to set style dynamically */}
+      <StatusBar style='dark' />
+      <Stack screenOptions={screenOptions}>
+        <Stack.Screen name="(home)" />
+      </Stack>
+    </>)
 }
 
 const screenOptions: NativeStackNavigationOptions = {
